55cdcfcffb77cc4df237c637c3af84b8c507f08d
Izhar Khan <itzizharkhan2@gmail.com>
2025-10-06 02:13:55 +0500

    solve the Import problem

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -4,14 +4,14 @@ const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: Number, required: true, min: 0 }, // Add min validation
-    offerPrice: { type: Number, required: true, min: 0 }, // Add min validation
-    image: { type: [String], required: true }, // Specify array of strings
-    category: { type: [String], required: true }, // Specify array of strings
+    price: { type: Number, required: true, min: 0 },
+    offerPrice: { type: Number, required: true, min: 0 }, 
+    image: { type: [String], required: true }, 
+    category: { type: [String], required: true }, 
     inStock: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.models.Product || mongoose.model('Product', productSchema);
-// export default Product;
+const Product =  mongoose.models.Product || mongoose.model('Product', productSchema);
+export default Product;
